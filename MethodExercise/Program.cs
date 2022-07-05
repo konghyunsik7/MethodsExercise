d0afe90b8cfc47b78a69a58f50ddaf8cb5de44c2
Hyun Kong <konghyunsik7@gmail.com>
2022-07-05 11:22:32 -0400

    inital

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("What is your name?"); //output
+
+var name = Console.ReadLine();
+
+Console.WriteLine("What is your fav color?"); //output
+
+var color = Console.ReadLine();
+
+Console.WriteLine("What is your fav animal?"); //output
+
+var animal = Console.ReadLine();
+
+Console.WriteLine("What is your fav band?"); //output
+
+var band = Console.ReadLine();
+
+
+Console.WriteLine($"Hello, {name}. I like the color of your {color} skin. I heard you were the new {animal}dad of your community. {band} would be very proud");
